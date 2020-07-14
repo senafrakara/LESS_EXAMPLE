0ba876a8d8e9bc84a6ee1687428af383baaecf89
Nursena Karakulah <senafrakara@gmail.com>
2020-07-14 16:01:28 +0300

    Add project files.

---
+++ LESS_EXAMPLE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LESS_EXAMPLE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
