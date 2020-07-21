0f45a2a4db341700edf188dbdbd4026c7a0eff85
Nursena Karakulah <senafrakara@gmail.com>
2020-07-21 14:16:06 +0300

    Slider, sidenavbar, navbar finished
    Slider, sidenavbar, navbar updated and finished

--- LESS_EXAMPLE/wwwroot/js/site.js
+++ LESS_EXAMPLE/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    navText: ["<div class='arrow-left icon'></div>", "<div class='arrow-right icon'></div>"],
+    nav: true,
+    animateIn: 'flipInX',
+    stagePadding: 100,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
